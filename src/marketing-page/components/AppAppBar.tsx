e615e7c3844cbe08492e3a578f2baedddc59b465
Prajwal Bhati <prajwalbhati@hotmail.com>
2026-03-26 14:36:09 +0530

    fix: remove unused imports

--- src/marketing-page/components/AppAppBar.tsx
+++ src/marketing-page/components/AppAppBar.tsx
@@ -6,7 +6,6 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import Container from '@mui/material/Container';
-import Divider from '@mui/material/Divider';
 import MenuItem from '@mui/material/MenuItem';
 import Drawer from '@mui/material/Drawer';
 import MenuIcon from '@mui/icons-material/Menu';
